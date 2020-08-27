718f8e7ac422e3da69dabba060877bd07e453443
Fabio Eufrazio Jr <fabioeufrazio91@gmail.com>
2020-08-27 09:26:01 -0300

    Add aula04 and ExercicioParte II

---
+++ aula-04/src/br/edu/univas/main/StartApp.java
@@ -0,0 +1,15 @@
+package br.edu.univas.main;
+
+import java.util.Scanner;
+
+public class StartApp {
+	
+	public static void main(String [] args ) {
+		
+		Scanner leitura = new Scanner (System.in);
+		
+		int [] notas = new int [10];
+		
+	}
+
+}
