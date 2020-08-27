1e63c748d086ce03ecd8f292c51c7c23743ddd9d
Fabio Eufrazio Jr <fabioeufrazio91@gmail.com>
2020-08-27 10:01:02 -0300

    AlteraçãoAula04

--- aula-04/src/br/edu/univas/main/StartApp.java
+++ aula-04/src/br/edu/univas/main/StartApp.java
@@ -3,13 +3,41 @@ package br.edu.univas.main;
 import java.util.Scanner;
 
 public class StartApp {
-	
-	public static void main(String [] args ) {
+
+	public static void main(String[] args) {		
+		float[] notas = leituraNotas();
+
+		ajustaNota(notas);
 		
-		Scanner leitura = new Scanner (System.in);
-		
-		int [] notas = new int [10];
-		
+		impressaoNotas(notas);
 	}
 
+	public static float[] leituraNotas() {
+		Scanner leitura = new Scanner(System.in);
+		
+		float[] notas = new float[10];
+		
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println("Digite a nota: ");
+			notas[i] = leitura.nextInt();
+		}
+		
+		leitura.close();
+		return notas;
+	}
+	
+	public static void ajustaNota(float[] notasDigitadas) {
+		for (int i = 0; i < notasDigitadas.length; i++) {
+			notasDigitadas[i] *= 1.5f;
+		}
+	}
+	
+	public static void impressaoNotas(float[] notas) {
+		System.out.println("\n\nNotas digitadas:");
+		
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println((i + 1) + "ª nota: " + notas[i]);
+		}
+	}
+	
 }
